b6d0c61449beff990e2d7d84baaa0994cfa612d4
nhyilin <nhyilin@foxmail.com>
2022-07-05 10:58:22 +0800

    delete useless print

--- code/Python/statisticalIntervals/main.py
+++ code/Python/statisticalIntervals/main.py
@@ -5,22 +5,21 @@ import xlrd
 def linear_filter(score_list):
     step = 0.01
     for k, g in groupby(sorted(score_list), key=lambda x: x // step):
-        print('{}-{}: {}'.format(k * step, (k + 1) * step + 1, len(list(g))))
+        print('{}-{}: {}'.format(k * step, (k + 0.01) * step + 0.01, len(list(g))))
 
 
 def read_xlrd():
     global cols
     worksheet = xlrd.open_workbook('./data.xlsx')
     sheet_names = worksheet.sheet_names()
-    print(sheet_names)
+    # print(sheet_names)
     for sheet_name in sheet_names:
         sheet = worksheet.sheet_by_name(sheet_name)
 
         cols = sheet.col_values(3)
         del cols[0]
-        print(cols)
-        print(type(cols[3]))  # 查看数据类型
-        print(type(cols))  # 查看数据类型
+        # print(type(cols[3]))  # 查看数据类型
+        # print(type(cols))  # 查看数据类型
     return cols
 
 
