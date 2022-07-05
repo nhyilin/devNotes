19b03a743cda0c034c5ff3daab97c66ad6c2e860
nhyilin <nhyilin@foxmail.com>
2022-07-05 10:42:31 +0800

    解析.xlsx文件并按区间统计落点个数

---
+++ code/Python/statisticalIntervals/main.py
@@ -0,0 +1,28 @@
+from itertools import groupby
+import xlrd
+
+
+def linear_filter(score_list):
+    step = 0.01
+    for k, g in groupby(sorted(score_list), key=lambda x: x // step):
+        print('{}-{}: {}'.format(k * step, (k + 1) * step + 1, len(list(g))))
+
+
+def read_xlrd():
+    global cols
+    worksheet = xlrd.open_workbook('./data.xlsx')
+    sheet_names = worksheet.sheet_names()
+    print(sheet_names)
+    for sheet_name in sheet_names:
+        sheet = worksheet.sheet_by_name(sheet_name)
+
+        cols = sheet.col_values(3)
+        del cols[0]
+        print(cols)
+        print(type(cols[3]))  # 查看数据类型
+        print(type(cols))  # 查看数据类型
+    return cols
+
+
+if __name__ == '__main__':
+    linear_filter(read_xlrd())
