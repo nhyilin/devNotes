938bb9e1762c91a379d24de3b682236b175fe4d6
nhyilin <nhyilin@foxmail.com>
2022-07-07 22:18:14 +0800

    修改mu参数

--- code/Python/draw_function_of_two_variables_/main.py
+++ code/Python/draw_function_of_two_variables_/main.py
@@ -7,11 +7,13 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # 论文中为投掷后的分数与投掷角度、速度的函数
 # 本文为速度与摩擦系数、角度的函数
+
 M = float(19.96)
 G = float(9.8)
-MU = np.arange(0.05, 0.1, 0.01).tolist()
+# MU = [0.0625, 0.25, 0.5]
+MU = [1, 2, 3]
 V = np.arange(1.8, 3.2, 0.01).tolist()
-THETA = np.arange(-0.06 * np.pi, 0.06 * np.pi, 0.02).tolist()
+THETA = np.arange(-0.06 * np.pi, 0.06 * np.pi, 0.002).tolist()
 YITA = random.random()
 
 
@@ -33,9 +35,20 @@ for i in range(len(MU)):
         RESULT.append(V[j])
         RESULT.append(cal_integration(V[j], MU[i]))
 RESULT = np.array(RESULT).reshape(len(MU) * len(V), 3)
-RESULT_MU = [RESULT[:, 0]]
-RESULT_V = [RESULT[:, 1]]
-RESULT_F = [RESULT[:, 2]]
+print(RESULT, "\n -------------------RESULT-------------------")
+# RESULT_MU = RESULT[:, 0]
+# RESULT_V = RESULT[:, 1]
+# RESULT_F = RESULT[:, 2]
+
+RESULT_01 = RESULT[0: len(V)]  # 截取V长度的计算结果作为mu01参数，此处长度计算应为len(V)*len(MU)/3，但是语法好像不支持，正好len(MU)=3，所以这么写
+RESULT_02 = RESULT[len(V): 2 * len(V)]
+RESULT_03 = RESULT[2 * len(V): 3 * len(V)]
+print(RESULT_01, "\n -------------------RESULT_01-------------------")
+print(RESULT_02, "\n -------------------RESULT_02-------------------")
+print(RESULT_03, "\n -------------------RESULT_03-------------------")
+# RESULT_MU = RESULT_MU.reshape(3, len(RESULT)/3)
+
+# print([RESULT_MU[:,len(RESULT_V)]])#RESULT_MU_01
 # print(RESULT)
 # print(shape(RESULT))
 
@@ -51,7 +64,7 @@ RESULT_F = [RESULT[:, 2]]
 # ax = Axes3D(fig, auto_add_to_figure=False)
 # fig.add_axes(ax)
 #
-# ax.scatter(RESULT_MU, RESULT_V, RESULT_F, c='r', marker='*')
+# ax.scatter(RESULT_MU, RESULT_V, RESULT_F, c='r')#, marker='*'
 #
 # ax.set_xlabel('MU label')  # 画出坐标轴
 # ax.set_ylabel('V label')
@@ -59,12 +72,13 @@ RESULT_F = [RESULT[:, 2]]
 # plt.show()
 
 
+plt.style.use('ggplot')
+plt.figure('relation between F & mu & v')
 
-# 创建绘图图表对象，可以不显式创建，跟cv2中的cv2.namedWindow()用法差不多
-plt.figure('Draw')
+plt.plot(RESULT_01[:, 1], RESULT_01[:, 2], 'ro') # 绘制 RESULT_01 的后两列，分别为 V 和 F
+plt.plot(RESULT_02[:, 1], RESULT_02[:, 2], 'b') # MU2
+plt.plot(RESULT_03[:, 1], RESULT_03[:, 2], 'g') # MU3
+plt.style.use('ggplot')
 
-plt.plot(RESULT_V, RESULT_F, 'ro')
 plt.draw()  # 显示绘图
-
-# plt.savefig("easyplot.jpg")  # 保存图象
 plt.show()
