e10e53ad469a9cfa6bc5a49f7444977874cb3e1c
nhyilin <nhyilin@foxmail.com>
2022-09-19 23:01:25 +0800

    add readme file

---
+++ code/cpp/DataStructure/LinkedList.h
@@ -0,0 +1,28 @@
+//
+// Created by 裴奕霖 on 2022/9/19.
+//
+
+#ifndef DATASTRUCTURE_LINKEDLIST_H
+#define DATASTRUCTURE_LINKEDLIST_H
+
+
+//class LinkedList {
+//
+//};
+
+//*************静态顺序表*************//
+
+struct Sqlist{
+
+};
+
+
+//*************动态顺序表*************//
+struct Selist{
+    int *data;
+    int MaxSize;
+    int length;
+};
+
+
+#endif //DATASTRUCTURE_LINKEDLIST_H
--- code/cpp/DataStructure/main.cpp
+++ code/cpp/DataStructure/main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
-#include "Structs.h"
-#define InitSize 10
+//#include "Structs.h"
 
-void InitSqlist(Sqlist&L){
-    L.data =new int[InitSize];
-    L.length=0;
-    L.MaxSize=InitSize;
-}
-void IncreaseSize(Sqlist&L,int l){
-    int *p=L.data;
-    L.data = new int[InitSize+l];
-    for(int i=0;i<InitSize+l;i++)
-        L.data[i]=p[i];
-    delete[] p;
-}
+
+
 int main(){
 
 
