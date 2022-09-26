47523a1b5acfc2209bba58a3cf47790df90a2945
nhyilin <nhyilin@foxmail.com>
2022-09-26 19:38:06 +0800

    update

--- code/cpp/DataStructure/StackAndQueue.cpp
+++ code/cpp/DataStructure/StackAndQueue.cpp
@@ -17,11 +17,39 @@ bool Empty(SqStack& S) {
         return false;
 }
 bool Push(SqStack& S, ElemType e) {
+    // 进栈操作
     if (S.top = MaxSize - 1) return false;
-    S.top++;//TODO 易错，指针先上移，再赋值
+    S.top++;  // TODO 易错，指针先上移，再赋值
     S.data[S.top] = e;
     return true;
 }
+bool Pop(SqStack& S, ElemType e) {
+    if (S.top == -1) return false;
+    e = S.data[S.top];  // 先出栈，指针再下移
+    S.top--;
+    return true;
+}
+
+bool GetTop(SqStack& S, ElemType e) {
+    if (S.top == -1) return false;
+    e = S.data[S.top];  // e记录栈顶元素
+    return true;
+}
+
+namespace Sharing_Stack {
+// 共享栈
+// 两个栈共享一片空间
+struct ShStack {
+    ElemType data[MaxSize];
+    int top0;  // 0号栈栈顶指针
+    int top1;  // 1号栈栈顶指针
+    //栈满条件: top0 + 1 = top1
+};
+void InitStack(ShStack& S) {
+    S.top0 = -1;
+    S.top1 = MaxSize;
+}
+}  // namespace Sharing_Stack
 }  // namespace Sequence_Stack
 
 }  // namespace  stack_and_queue
