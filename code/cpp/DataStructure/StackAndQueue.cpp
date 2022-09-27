d572733f294d18ffff534f622ad0edfd6bc0ede8
nhyilin <nhyilin@foxmail.com>
2022-09-27 11:29:01 +0800

    Update StackAndQueue.cpp

--- code/cpp/DataStructure/StackAndQueue.cpp
+++ code/cpp/DataStructure/StackAndQueue.cpp
@@ -152,9 +152,27 @@ bool IsEmpty(const LinkQueue& Q) {
     else
         return false;
 }
-
+void EnQueue(LinkQueue& Q, ElemType& x) {
+    LinkNode* s = new LinkNode;
+    s->data = x;
+    s->next = nullptr;  // TODO 很容易漏
+    Q.rear->next = s;
+    Q.rear = s;  // TODO 很容易漏
+}
+bool DeQueue(LinkQueue& Q, ElemType& x) {
+    // 队头元素出队
+    // TODO
+    // 易错，要搞清楚出列时，出的是哪一个节点，不能把头节点当成第一个元素移出去
+    if (Q.rear == Q.front) return false;
+    LinkNode* p = Q.front->next;  // 这里Q.front其实是头节点
+    x = p->data;
+    Q.front->next=p->next;
+    if (Q.front==p)
+        Q.front=Q.rear;
+    delete p;
+    return true;
+}
 namespace linked_queue_nohead {
-
 void InitQueue(LinkQueue& Q) {
     // 初始化队列(不带头节点)
     Q.front = nullptr;
@@ -168,6 +186,22 @@ bool IsEmpty(const LinkQueue& Q) {
         return false;
 }
 
+void EnQueue(LinkQueue& Q, ElemType& x) {
+    // TODO 易漏
+    LinkNode* s = new LinkNode;
+    s->data = x;
+    s->next = nullptr;
+    if (Q.front == nullptr) {
+        Q.rear = s;
+        Q.front = s;
+    } else {
+        Q.rear->next = s;
+        Q.rear = s;
+    }
+}
+void DeQueue(LinkQueue&Q,ElemType&x){
+    
+}
 }  // namespace linked_queue_nohead
 }  // namespace linked_queue
 
