bb294b7d99b51131ee0f7c513eeb1c56b6196310
nhyilin <nhyilin@foxmail.com>
2023-01-14 11:23:46 +0800

    日常维护

--- DataStructure/DSACPP/Chap1_CPPReview/first.h
+++ DataStructure/DSACPP/Chap1_CPPReview/first.h
@@ -4,6 +4,8 @@
 
 #ifndef DATASTRUCTURE_DSACPP_CHAP1_CPPREVIEW_FIRST_H_
 #define DATASTRUCTURE_DSACPP_CHAP1_CPPREVIEW_FIRST_H_
+#include <algorithm>
+#include <numeric>
 // 1. x,y均为形式参数，函数内实参为复制得来，而非参数本体
 void swap(int& x, int& y) {
     x = x ^ y;
@@ -65,5 +67,89 @@ int mismatch(const T& list_a, const T& list_b, int max) {
         } else
             return i + 1;
     }
+    return -1;
+}
+
+// 程序1-10 为一个二维数组分配存储空间
+template <class T>
+bool make2dArray(T**& x, int numberOfRows, int numberOfColumns) {
+    try {
+        // 创建行指针
+        x = new T*[numberOfRows];
+        // 为每一行分配空间
+        for (int i = 0; i < numberOfRows; i++) x[i] = new int[numberOfColumns];
+        return true;
+    } catch (const std::bad_alloc&) {
+        return false;
+    }
+}
+// 程序1-12 释放在函数make2dArray中分配的空间
+template <class T>
+void delete2dArray(T**& x, int numberOfRows) {
+    // 删除二维数组x
+    // 删除行数组空间
+    for (int i = 0; i < numberOfRows; i++) delete[] x[i];
+
+    // 删除行指针
+    delete[] x;
+    x = nullptr;
+}
+
+// 12
+template <class T>
+void myMake2dArray(T**& x, int numberOfRows, T* rowSize) {
+    x = new T*[numberOfRows];
+    for (int i = 0; i < numberOfRows; ++i) {
+        rowSize = new T[rowSize[i]];
+    }
+}
+template <class T>
+void myDelete2dArray(T**& x, int numberOfRows, T* rowSize) {
+    for (int i = 0; i < numberOfRows; i++) delete[] x[i];
+    delete[] x;
+    x = nullptr;  // 这里删除之后还要设置为空不太理解...
+}
+
+// 13
+template <class T>
+void changeLength1D(T*& a, int oldLength, int newLength) {
+    if (newLength < 0) std::cerr << "new length must be >= 0";
+
+    T* temp = new T[newLength];                   // new array
+    int number = std::min(oldLength, newLength);  // number to copy
+    copy(a, a + number, temp);
+    delete[] a;  // deallocate old memory
+    a = temp;
+}
+// 14
+template <class T>
+T* changeLength2D(T*& a, int old_x, int old_y, int new_x, int new_y) {
+    if (new_x < 0 || new_y < 0) std::cerr << "new length must be >= 0";
+
+    int** new_array;
+    make2dArray(new_array, new_x, new_y);
+
+    for (int i = 0; i < std::min(old_x, new_x); ++i) {
+        for (int j = 0; j < std::min(old_y, new_y); ++j) {
+            new_array[i][j] = a[i][j];
+        }
+    }
+    delete2dArray(a, old_x);
+    return new_array;
+}
+// 程序1-33
+template <class T>
+T sum(T* a, int n) {
+    // stl accumulate算法
+    T theSum = 0;
+    return std::accumulate(a, a + n, theSum);
+}
+
+// 程序1-34
+template <class T>
+T Prodect(T* a, int n) {
+    // stl Prodect算乘法
+    T prodect = 1;
+    return std::accumulate(a, a + n, prodect,std::multiplies<T>());
 }
 #endif  // DATASTRUCTURE_DSACPP_CHAP1_CPPREVIEW_FIRST_H_
--- DataStructure/DSACPP/Chap1_CPPReview/test_first.cpp
+++ DataStructure/DSACPP/Chap1_CPPReview/test_first.cpp
@@ -2,48 +2,69 @@
 // Created by 裴奕霖 on 2022/12/22.
 //
 #include "first.h"
+void devideAnswers(int questions) {
+    std::cout << "**************\n";
+    std::cout << "// " << questions << "\n";
+}
 int main() {
     // 1
+    devideAnswers(1);
     int a = 0, b = 9;
     swap(a, b);
-    std::cout << "// 1\n";
-    std::cout << "**************\n";
+
     std::cout << "a: " << a << "\t b: " << b << std::endl;
-    std::cout << "**************\n";
+
     // 2
-    std::cout << "// 2\n";
+    devideAnswers(2);
     int int_list[] = {1, 2, 3, 4, 9};
     double double_list[] = {1.2, 3.2, 7.9, 9.0, 8.99, 9.02};
     std::cout << count(int_list) << std::endl;
     std::cout << count(double_list) << std::endl;
-    std::cout << "**************\n";
 
     // 3
-    std::cout << "// 3\n";
+    devideAnswers(3);
     double x = 9.9;
     fill(double_list, 6, x);
     for (auto iter : double_list) {
         std::cout << iter << std::endl;
     }
-    std::cout << "**************\n";
 
     // 4
-    std::cout << "// 4\n";
+    devideAnswers(4);
     double list01[] = {1.2, 2.1, 3.4, 5.4};
     double list02[] = {9.2, 2.7, 1.4, 0.4};
     std::cout << "sum: " << inner_prodcut(list01, list02, 4) << std::endl;
-    std::cout << "**************\n";
 
     // 6
-    std::cout << "// 6\n";
+    devideAnswers(6);
     std::cout << "is_sorted: " << is_sorted(list01, 4) << std::endl;
     std::cout << "is_sorted: " << is_sorted(list02, 4) << std::endl;
-    std::cout << "**************\n";
 
-    //7
-    std::cout << "// 7\n";
+    // 7
+    devideAnswers(7);
     double list01_7[] = {1.2, 2.1, 3.4, 5.4};
     double list02_7[] = {1.2, 2.12, 3.4, 5.5};
-    std::cout << "first mismatch element: " << mismatch(list01_7,list02_7, 4) << std::endl;
-    std::cout << "**************\n";
+    std::cout << "first mismatch element: " << mismatch(list01_7, list02_7, 4)
+              << std::endl;
+
+    // 12
+    devideAnswers(12);
+    int **myArray = nullptr;
+    int rowSize[] = {1, 2, 3, 5};
+    myMake2dArray(myArray, 9, rowSize);
+
+    // 13
+    devideAnswers(13);
+    int devideAnswers_13[] = {1, 2, 3, 5};
+    //    changeLength1D(devideAnswers_13, 4,6);//TODO 这里调用方法还不确定
+    for (int i : devideAnswers_13) std::cout << i << std::endl;
+
+    // 1-33
+    devideAnswers(33);
+    std::cout << sum(devideAnswers_13, 4);
+
+    devideAnswers(34);
+    std::cout<<Prodect(devideAnswers_13,4);
+
+//    std::cin.get();
 }
