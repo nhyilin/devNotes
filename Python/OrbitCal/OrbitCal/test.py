af1df01e87664d9232f6418a73532ad476cebabc
Yilin Pei <nhyilin@foxmail.com>
2024-05-14 15:49:43 +0800

    SPICE库，python版本，轨道预测

---
+++ Python/OrbitCal/OrbitCal/test.py
@@ -0,0 +1,14 @@
+from pykep import planet
+
+# 轨道六根数 (假定单位：AU和度数)
+semimajor_axis = 1.0  # 半长轴 (天文单位)
+eccentricity = 0.0167  # 偏心率
+inclination = 0.00005  # 倾角 (度数)
+RAAN = -11.26064  # 升交点经度 (度数)
+argument_of_periapsis = 114.20783  # 近点角 (度数)
+mean_anomaly = 358.617  # 平近点角 (度数)
+
+# 参考时刻，J2000
+# ref_epoch = epoch(0)  # J2000
+
+print("hello world")
